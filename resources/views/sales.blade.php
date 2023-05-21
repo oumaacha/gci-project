cac3a00eab166e527f9e34622664e6d5f853ac2e
Anouar Oumaacha <61355340+oumaacha@users.noreply.github.com>
2023-05-21 21:44:08 +0100

    GP-21

--- resources/views/sales.blade.php
+++ resources/views/sales.blade.php
@@ -10,4 +10,38 @@
 @endsection
 @section('cardBody')
     <h1>Sales Page</h1>
+    <table class="table table-bordered">
+    <tr>
+        <td class="w-25">Référence</td>
+        <td><b>14575</b></td>
+    </tr>
+    <tr>
+        <td class="w-25">Désignation</td>
+        <td>Produit 1</td>
+    </tr>
+    <tr>
+        <td class="w-25">Prix unitaire</td>
+        <td><b>150 MAD</b></td>
+    </tr>
+    <tr>
+        <td class="w-25">Quantité</td>
+        <td><b>20 unités</b></td>
+    </tr>
+    <tr>
+        <td class="w-25">TVA</td>
+        <td><b>20 %</b></td>
+    </tr>
+    <tr>
+        <td class="w-25">Dernière modification</td>
+        <td>21/05/2023</td>
+    </tr>
+    <tr>
+        <td class="w-25">Chiffre d'affaire</td>
+        <td><b>1450 MAD</b></td>
+    </tr>
+    <tr>
+        <td class="w-25">Description</td>
+        <td>this is description</td>
+    </tr>
+</table>
 @endsection
