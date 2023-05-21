32af889b67dbc975548a0c33ed7b015d92ad3b45
Ayoub9LE <103582597+Ayoub9LE@users.noreply.github.com>
2023-05-21 20:36:31 +0000

    GP-19

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -1,7 +1,39 @@
 <!doctype html>
 <html lang="en">
 <head>
+<style>
+        .search-container {
+  position: relative;
+  display: inline-block;
+}
+
+.search-container input[type="text"] {
+  padding: 10px;
+  border: none;
+  border-radius: 5px;
+  font-size: 16px;
+  outline: none;
+}
+
+.search-container button[type="submit"] {
+  position: absolute;
+  top: 0;
+  right: 0;
+  padding: 10px;
+  background-color: #333;
+  border: none;
+  border-radius: 0 5px 5px 0;
+  color: #fff;
+  cursor: pointer;
+}
+
+.search-container button[type="submit"]:hover {
+  background-color: #555;
+}
+
+    </style>
     <meta charset="UTF-8">
+    
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
@@ -21,6 +53,10 @@
             </div>
         </div>
     </div>
+    <div class="search-container">
+        <input type="text" placeholder="Search...">
+        <button type="submit"><i class="fa fa-search"></i></button>
+      </div>
 @yield('script')
 </body>
 </html>
