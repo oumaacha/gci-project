4efabda34dd9e2d3a664cb76119cac3e84e7159b
winmoh <103373835+winmoh@users.noreply.github.com>
2023-05-21 22:40:41 +0100

    GP-46

---
+++ resources/views/profile.balde.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>User Profile</title>
+  <style>
+    body {
+      font-family: Arial, sans-serif;
+    }
+    .profile {
+      max-width: 600px;
+      margin: 0 auto;
+      padding: 20px;
+      background-color: #f4f4f4;
+      border: 1px solid #ddd;
+    }
+    .profile h1 {
+      text-align: center;
+    }
+    .profile img {
+      display: block;
+      width: 150px;
+      height: 150px;
+      margin: 0 auto;
+      border-radius: 50%;
+      object-fit: cover;
+    }
+    .profile p {
+      text-align: center;
+    }
+  </style>
+</head>
+<body>
+  <div class="profile">
+    <img src="profile-picture.jpg" alt="Profile Picture">
+    <h1>John Doe</h1>
+    <p>Email: john.doe@example.com</p>
+    <p>Location: New York, USA</p>
+    <p>Bio: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam blandit est eu mauris accumsan, ut interdum magna mattis.</p>
+  </div>
+</body>
+</html>
