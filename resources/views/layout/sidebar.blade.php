a1817f5bfbc43e8499e5e025ff1fc71130b3d39f
noro129 <109439099+noro129@users.noreply.github.com>
2023-05-21 22:23:36 +0100

    GP-34

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -6,7 +6,10 @@
 <hr class="solid">
 
 <div class="d-flex flex-column gap-2 p-1">
-    <div class="sidebar-element">Dashboard</div>
-    <div class="sidebar-element">Produits</div>
-    <a class="text-decoration-none text-black" href="{{url('prod/vendre')}}"><div class="sidebar-element">Vendre</div></a>
+    <div>
+        <div class="sidebar-element">Dashboard</div>
+        <div class="sidebar-element">Produits</div>
+        <a class="text-decoration-none text-black" href="{{url('prod/vendre')}}"><div class="sidebar-element">Vendre</div></a>
+    </div>
+    <button class="btn btn-primary">Dark Mode</button>
 </div>
